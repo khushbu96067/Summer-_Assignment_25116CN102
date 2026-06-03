5574d7f3975a904e3e591486c450cb9132d2393a
khushbu96067 <khushbukumari9606@gmail.com>
2026-06-03 16:53:47 +0530

    Add files via upload

---
+++ Q10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int start,end,i,j,prime;
+
+    printf("Enter start and end of range:");
+    scanf("%d %d", &start, &end);
+    printf("prime numbares are:\n");
+
+    for(i=start, i<=end; i++)
+    {
+        if(i<2)
+        continue;
+
+        prime=1;
+
+        for(j=2; j<=i/2; j++)
+        {
+            if(i%j==0)
+        {
+            prime=0;
+            break;
+        }
+    }
+    if(prime)
+    printf("%d",i);
+}
+return 0;
+}
